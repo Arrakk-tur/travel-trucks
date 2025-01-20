f81ac5465f12315c37f377b56d407c4ba94e73de
Kostiantyn Skliar <skljar.k@gmail.com>
2025-01-20 21:06:10 +0100

    Structure2

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Home from "./pages/Home";
 import Catalog from "./pages/Catalog";
 import CamperDetails from "./pages/CamperDetails";
 import Header from "./components/Header";
-import Footer from "./components/Footer";
 import styles from "./styles/App.module.css";
 
 function App() {
@@ -18,7 +17,6 @@ function App() {
             <Route path="/catalog/:id" element={<CamperDetails />} />
           </Routes>
         </main>
-        <Footer />
       </div>
     </Router>
   );
